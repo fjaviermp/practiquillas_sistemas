32da3779ecdf8a487731e8a445cdea4dc08b6826
Francisco Javier Martínez Plaza <74726752+fjaviermp@users.noreply.github.com>
2021-02-25 13:05:03 +0100

    0.1

--- Ejercicios de introducción a SHELL/14.sh
+++ Ejercicios de introducción a SHELL/14.sh
@@ -4,9 +4,9 @@
 #!/bin/bash
 A1=""
 
-test -z $A1
-test -z "$A1"
-test -n $A1
-test -n "$A1"
-test ! -n $A1
-test ! -n "$A1"
+test -z $A1 && echo "A SI $A1"
+test -z "$A1" && echo "B SI $A1"
+test -n $A1 && echo "C SI $A1"
+test -n "$A1" && echo "D SI $A1"
+test ! -n $A1 && echo "E SI $A1"
+test ! -n "$A1" && echo "F SI $A1"
